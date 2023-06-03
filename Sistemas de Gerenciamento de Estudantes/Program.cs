91895d776ab286b55321db11549c972eae63622f
Gabriel <gabrielwerta@outlook.com>
2023-06-03 15:25:23 -0300

    Projeto quase completo faltando apenas algumas melhorias para tratar alguns erros que podem acontecer se o usário colocar entradas de tipos diferentes do que estar sendo pedido.

--- Sistemas de Gerenciamento de Estudantes/Program.cs
+++ Sistemas de Gerenciamento de Estudantes/Program.cs
@@ -1,38 +1,89 @@
 ﻿using Sistema_de_Gerenciamento_de_Estudantes.Aluno;
 
-Console.WriteLine("--------------------------");
-Console.WriteLine("GERENCIADOR DE ESTUDANTES");
-Console.WriteLine("--------------------------");
-int opcao = -1;
-do
+public class Program
 {
-    Console.WriteLine("[1]Adicionar aluno\n[2]Exibir lista de alunos\n[0]Encerrar programa");
-    Console.Write("--------------------------\nEscolha uma opção: ");
-    opcao = int.Parse(Console.ReadLine());
-    switch (opcao)
+    static List<Aluno> alunos = new List<Aluno>();
+    public static void Main(string[] args)
     {
-        case 1:
-            AdicionarAluno();
-            break;
-        case 2:
-            ExibirListaDeAlunos();
-            break;  
-        case 0:
-            Console.WriteLine("Consulta finalizada");
-            break;
+        Console.WriteLine("--------------------------");
+        Console.WriteLine("GERENCIADOR DE ESTUDANTES");
+        
+        int opcao = -1;
+        do
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("[1]Adicionar aluno\n[2]Exibir lista de alunos\n[0]Encerrar programa");
+            Console.Write("--------------------------\nEscolha uma opção: ");
+            opcao = int.Parse(Console.ReadLine());
+            switch (opcao)
+            {
+                case 1:
+                    AdicionarAluno();
+                    break;
+                case 2:
+                    ExibirListaDeAlunosEInfos();
+                    break;  
+                case 0:
+                    Console.WriteLine("Consulta finalizada");
+                    break;
 
-        default:
-            Console.WriteLine("Opção inválida, tente novamente.");
-            break;
+                default:
+                    Console.WriteLine("Opção inválida, tente novamente.");
+                    break;
+            }
+
+        } while (opcao != 0);
+
+        static void AdicionarAluno()
+        {
+            Aluno novoAluno = new Aluno();
+            Console.Write("Nome do aluno: ");
+            novoAluno.Nome = Console.ReadLine();
+            Console.Write("Idade do aluno: ");
+            novoAluno.Idade = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Digite a "+(i+1)+"a nota: ");
+                novoAluno.Notas[i] = double.Parse(Console.ReadLine());
+            }
+            alunos.Add(novoAluno);
+        }
+        static void ExibirListaDeAlunosEInfos()
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Lista de alunos: ");
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno.Nome);
+            }
+            Console.WriteLine("------------------------------------------------------");
+            Console.Write("Digite o nome do aluno para mostrar suas informações: ");
+            String alunoEscolhido = Console.ReadLine();
+            
+            Console.WriteLine("---------------------------");
+            MostrarInfosDoAluno(alunoEscolhido);
+        }
+        
+        static void MostrarInfosDoAluno(string nomeAlunoEscolhido)
+        {
+            foreach (var aluno in alunos)
+            {
+                if (nomeAlunoEscolhido == aluno.Nome)
+                {
+                    Console.WriteLine("Nome: " + aluno.Nome);
+                    Console.WriteLine("Idade: " + aluno.Idade);
+                    for (int i = 0; i < 3; i++)
+                    {
+                        Console.WriteLine("Nota "+(i+1)+": " + aluno.Notas[i]);  
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Nome de aluno inválido, tente nomes que estão na lista!");
+                }
+            }
+        }
     }
-
-} while (opcao != 0);
-
-static void AdicionarAluno()
-{
-
-}
-static void ExibirListaDeAlunos()
-{
-
 }
